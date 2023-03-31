a72c82c06f1d9f2dded42264fbbfe9acc5ad62c7
Chirag Simkhada <chiragsimkhada@gmail.com>
2023-03-31 20:00:07 +0545

    Javascript

---
+++ JavaScript/script.js
@@ -0,0 +1,47 @@
+// console.log("Hello World");
+// alert("Some Alert");
+
+// let var1 = prompt("Enter any Number")
+
+// let const var
+
+const abc = 100; // Cannot change once Declared
+let var2 = 500; // Block Level Scope
+var var3 = 600; // Function Level Scope
+
+// console.log(abc)
+
+// console.log(var1)
+
+
+// Data Types (String, Boolean, Numbers, Array, Objects)
+
+// let var3 = "Abcd" // String
+let var4 = 100 // Number
+let var5 = true // Boolean true/false
+
+let a = 10
+let b = "2"
+
+// console.log(var4 + a + b)
+// PEMDAS
+// console.log(a / b)
+
+let arr1 = [56, 78, 44, 68, 89, 342, 123123, 324, 423, 4, 435234324]
+console.log(arr1)
+console.log(arr1.length)
+console.log(arr1.slice(0, 5))
+
+let obj1 = {
+    name: "Abc",
+    mark: 56
+}
+
+let obj2 = {
+    name: "Def",
+    mark: 78
+}
+
+console.log(obj1)
+
+// reverse, splice, pop, insert, push,.....
