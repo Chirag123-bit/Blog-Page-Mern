b85aaffc6663faea94b5d8c47d15be255af9d7c6
Chirag Simkhada <chiragsimkhada@gmail.com>
2023-04-02 20:02:27 +0545

    JS

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -5,43 +5,83 @@
 
 // let const var
 
-const abc = 100; // Cannot change once Declared
-let var2 = 500; // Block Level Scope
-var var3 = 600; // Function Level Scope
+// const abc = 100; // Cannot change once Declared
+// let var2 = 500; // Block Level Scope
+// var var3 = 600; // Function Level Scope
 
-// console.log(abc)
+// // console.log(abc)
 
-// console.log(var1)
+// // console.log(var1)
 
 
-// Data Types (String, Boolean, Numbers, Array, Objects)
+// // Data Types (String, Boolean, Numbers, Array, Objects)
 
-// let var3 = "Abcd" // String
-let var4 = 100 // Number
-let var5 = true // Boolean true/false
+// // let var3 = "Abcd" // String
+// let var4 = 100 // Number
+// let var5 = true // Boolean true/false
 
-let a = 10
-let b = "2"
+// let a = 10
+// let b = "2"
 
-// console.log(var4 + a + b)
-// PEMDAS
-// console.log(a / b)
+// // console.log(var4 + a + b)
+// // PEMDAS
+// // console.log(a / b)
 
-let arr1 = [56, 78, 44, 68, 89, 342, 123123, 324, 423, 4, 435234324]
-console.log(arr1)
-console.log(arr1.length)
-console.log(arr1.slice(0, 5))
+// let arr1 = [56, 78, 44, 68, 89, 342, 123123, 324, 423, 4, 435234324]
+// console.log(arr1)
+// console.log(arr1.length)
+// console.log(arr1.slice(0, 5))
 
-let obj1 = {
-    name: "Abc",
-    mark: 56
-}
+// let obj1 = {
+//     name: "Abc",
+//     mark: 56
+// }
 
-let obj2 = {
-    name: "Def",
-    mark: 78
-}
+// let obj2 = {
+//     name: "Def",
+//     mark: 78
+// }
 
-console.log(obj1)
+// console.log(obj1)
 
 // reverse, splice, pop, insert, push,.....
+
+// Array Methods (Push, pop, toString, join,
+//                shift, unshift, concat, splice, slice)
+
+
+// Heteregenous List
+// console.log(array)
+// array.push("Item Added")
+// console.log(array)
+// array.pop()
+// array.pop()
+// array.pop()
+// array.pop()
+// console.log(array)
+// array.shift()
+// array.unshift(100)
+// console.log(array)
+// console.log(array.slice(1, 4)) //Slices the array from index 1 to 3->(4-1)
+
+// array.splice(2, 2, "New Item", 50, 99, 88, 888) // Start, DeleteCount, ItemsToAdd
+// console.log(array)
+// console.log(array.length)
+// let newString = array.toString()
+// console.log(newString)
+
+// let newString2 = array.join(" ")
+// console.log(newString2)
+
+
+let array = [10, 50, 558, 6515, 0515, 5154] //6
+
+// Initilization; Condition; Increment/Decrement 
+// i++ = i+1
+
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i])
+}
+
+
+// Variable Pollution
