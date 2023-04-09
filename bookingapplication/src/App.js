d246206830568f001fc1593766a7bb159cddc434
Chirag Simkhada <chiragsimkhada@gmail.com>
2023-04-09 19:30:53 +0545

    Images

--- bookingapplication/src/App.js
+++ bookingapplication/src/App.js
@@ -1,11 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/common/navbar';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar/>
-    </div>
+   <BrowserRouter>
+   <NavBar/>
+
+    <Routes>
+      <Route path="/" element={<h1>HomePage</h1>} />
+      <Route path="/about" element={<h1>About Page</h1>} />
+      <Route path="/home" element={<h1>Home Page</h1>} />
+    </Routes>
+
+   </BrowserRouter>
   );
 }
 
